f0db08a436bfc595a2946e15aff5aa87b84e5cb4
Posi <andyade23@gmail.com>
2020-03-15 20:53:06 -0600

    Created a new file 'a8q1' and defined a new function 'subst()' with only a stub in it

---
+++ a8q1.py
@@ -0,0 +1,21 @@
+# OLAOLUWAPOSI ADEYEMI
+# 11267056
+# ooa642
+# CMPT 145- 02
+
+import treenode as tn
+import treefunctions as tf
+
+
+def subst(tnode, t, r):
+	"""
+	Purpose: To substitute a target value t with a replacement value r wherever it appears as a data value
+		in the given tree
+	Pre-conditions:
+		tnode: the given tree node
+		t: target value
+		r: replacement value
+	Post conditions: the target value is replaced with the replacement value
+	Return: None
+	"""
+	pass
---
+++ treefunctions.py
@@ -0,0 +1,55 @@
+# CMPT 145 Course material
+# Copyright (c) 2017-2020 Michael C Horsch
+# All rights reserved.
+#
+# This document contains resources for homework assigned to students of
+# CMPT 145 and shall not be distributed without permission.  Posting this 
+# file to a public or private website, or providing this file to a person 
+# not registered in CMPT 145, constitutes Academic Misconduct, according 
+# to the University of Saskatchewan Policy on Academic Misconduct.
+#
+
+
+
+import treenode as TN
+
+def is_leaf(tnode):
+    """
+    Purpose:
+        Determine if tnode is a leaf.
+    Pre-conditions:
+        :param tnode: a treenode
+    Return:
+        True if the tnode has zero children
+    """
+    return TN.get_left(tnode) is None and TN.get_right(tnode) is None
+
+
+def to_string(tnode, level=0):
+    """
+    Produce a formatted string to represent the hierarchy of
+    a tree.  Tree diagrams usually have the root at the top.
+    Here the root is at the top left.
+    - every data value appears on its own line
+    - the levels of a tree are columns from left to right
+    - nodes at the same level start in the same column
+    - very long data values might cause the presentation to get messy
+    - subtrees appear below a parent
+      - left subtree immediately
+      - right subtree after the entire left subtree
+    Pre-conditions:
+        :param tnode: a Binary tree (treenode or None)
+        :param level: the level of the tnode (default value 0)
+    Return:
+        A string with the hierarchy of the tree.
+    """
+    if tnode is None:
+        return 'EMPTY'
+    else:
+        result = '\t'*level
+        result += str(tn.get_data(tnode))
+        if tn.get_left(tnode) is not None:
+            result += '\n'+to_string(tn.get_left(tnode), level+1)
+        if tn.get_right(tnode) is not None:
+            result += '\n'+to_string(tn.get_right(tnode), level+1)
+        return result
